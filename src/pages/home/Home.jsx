fe454e5c271637b03b778d3f212937d9e438381c
Harshit <119065391+HarshitGururani@users.noreply.github.com>
2024-04-07 22:17:23 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import './style.scss';
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
-import Popular from './popular/popular';
+import Popular from './popular/Popular';
 import TopRated from './topRated/TopRated';
 Trending;
 const Home = () => {
